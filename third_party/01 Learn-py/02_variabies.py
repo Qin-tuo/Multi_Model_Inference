79b3b3866bde47649b5bb98a16275a05b57b3903
Qin-tuo <houxulongcz@gmail.com>
2026-09-01 15:03:43 +0800

    docs: add Python learning examples

---
+++ third_party/01 Learn-py/02_variabies.py
@@ -0,0 +1,28 @@
+# String variables
+greet="你好ya"
+greet_ch=greet
+greet_en="Hello"
+food = "rice"
+#print(f"greet_ch: {greet_ch}, greet_en: {greet_en}, food: {food}")
+
+#Integer variables
+age=18
+quantity=10
+num_of_students=30 
+print(f"You are {age} years old.")
+print(f"You have {quantity} items in your cart.")
+print(f"There are {num_of_students} students in the class.")
+
+#Float variables
+price=19.99
+GPA=3.5
+distance=5.2
+print(f"The price of the item is ${price}")
+print(f"Your GPA is {GPA}")
+print(f"You have walked {distance} miles.")
+
+#Boolean variables
+is_student=True
+# print(f"Are you a student? {is_student}")
+if is_student:
+    print("You are a student.")
